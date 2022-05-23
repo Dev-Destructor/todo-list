2ac54256c63f55c2e0a778edc1985cdedf415e75
Destructor007 <des578435@gmail.com>
2022-05-23 23:20:57 +0530

    components of the app

---
+++ client/src/components/TodoForm.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const TodoForm = (props) => {
+  const [input, setInput] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    // props.onSubmit({
+    //   id: Math.floor(Math.random() * 10000),
+    //   text: input,
+    // })
+
+    setInput('')
+  }
+
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  }
+  
+  return (
+    <div>
+      <form className="todo-form" onSubmit={handleSubmit}>
+        <input type="text" placeholder='add a todo' value={input} className='todo-input' onChange={handleChange} />
+        <button className='todo-button'>Add Todo</button>
+      </form>
+    </div>
+  )
+}
+
+export default TodoForm;
---
+++ client/src/components/TodoList.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+const TodoList = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default TodoList
